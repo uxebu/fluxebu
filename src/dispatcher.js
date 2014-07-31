27a0094c9737f0043a336d42badfb8ae297101d8
David Aurelio <dev@david-aurelio.com>
2014-07-31 22:22:53 +0200

    Add store synchronization to dispatcher

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -6,10 +6,26 @@ function Dispatcher() {
 
 Dispatcher.prototype = {
   dispatch: function(actionType, payload) {
+    function createProxy(storeName) {
+      var proxy = proxies[storeName];
+      if (proxy) {
+        return proxy;
+      }
+      return (proxies[storeName] = new ProxyStoreView());
+    }
+
+    function waitFor(storeName) {
+      return storeViews[storeName] || createProxy(storeName);
+    }
+
     var storeViews = {};
+    var proxies = {};
     var stores = this.stores;
     for (var key in stores) {
-      storeViews[key] = stores[key].notify(actionType, payload);
+      storeViews[key] = stores[key].notify(actionType, payload, waitFor);
+      if (proxies[key]) {
+        proxies[key].resolve(storeViews[key]);
+      }
     }
     return storeViews;
   },
@@ -18,6 +34,25 @@ Dispatcher.prototype = {
   },
   unsubscribeStore: function(name) {
     delete this.stores[name];
+  }
+};
+
+function ProxyStoreView() {
+  this.queries = [];
+  this.proxied = null;
+}
+ProxyStoreView.prototype = {
+  resolve: function(storeView) {
+    this.queries.forEach(storeView.query, storeView);
+    this.proxied = storeView;
+  },
+  query: function(callback) {
+    var proxied = this.proxied;
+    if (proxied) {
+      proxied.query(callback);
+    } else {
+      this.queries.push(callback);
+    }
   }
 };
 
--- test/dispatcher-test.js
+++ test/dispatcher-test.js
@@ -25,7 +25,7 @@ describe('dispatcher', function() {
     dispatcher.dispatch('arbitrary', null);
     expect(storeB.notify).not.toHaveBeenCalled();
   });
-  
+
   it('`dispatch()` returns an object that collects the return values of the notified stores', function() {
     dispatcher.subscribeStore('d', {notify: function() { return null; }});
 
@@ -35,6 +35,51 @@ describe('dispatcher', function() {
     expect(storeViews['c-store']).toBe(storeC.storeView);
     expect(storeViews.d).toBe(null);
   });
+
+  describe('synchronisation:', function() {
+    var storeA, storeB;
+    beforeEach(function() {
+      dispatcher = new Dispatcher();
+      storeA = asyncStore('A');
+      storeB = asyncStore('B');
+      dispatcher.subscribeStore('a', storeA);
+      dispatcher.subscribeStore('b', storeB);
+    });
+
+    it('synchronizes the second store to the first store', function(done) {
+      storeB.notify = function(actionType, payload, waitFor) {
+        return waitFor('a');
+      };
+      var storeViews = dispatcher.dispatch('arbitrary', null);
+      queryAandB(storeViews, function(values) {
+        expect(values.a).toBe(values.b);
+        done();
+      });
+    });
+
+    it('synchronizes the first store to the second store', function(done) {
+      storeA.notify = function(actionType, payload, waitFor) {
+        return waitFor('b');
+      };
+      var storeViews = dispatcher.dispatch('arbitrary', null);
+      queryAandB(storeViews, function(values) {
+        expect(values.a).toBe(values.b);
+        done();
+      });
+    });
+
+    function queryAandB(storeViews, callback) {
+      var values = {};
+      storeViews.a.query(function(value) {
+        values.a = value;
+        if (values.b) { callback(values); }
+      });
+      storeViews.b.query(function(value) {
+        values.b = value;
+        if (values.a) { callback(values); }
+      });
+    }
+  });
 });
 
 function mockStore() {
@@ -43,5 +88,17 @@ function mockStore() {
   return {
     notify: notify,
     storeView: storeView
+  };
+}
+
+function asyncStore(value) {
+  return {
+    notify: function() {
+      return {
+        query: function(callback) {
+          process.nextTick(function() { callback(value); });
+        }
+      };
+    }
   };
 }
