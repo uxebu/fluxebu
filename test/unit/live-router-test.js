fd4798618b51925a4fd8db0ebe48783d847c9192
David Aurelio <david@uxebu.com>
2014-08-01 17:55:20 +0200

    Add failing test for the live router

--- test/unit/live-router-test.js
+++ test/unit/live-router-test.js
@@ -1,7 +1,37 @@
 var LiveRouter = require('../../src/live-router');
 var MockDispatcher = require('../mock/dispatcher');
-
+var MockStoreResponse = require('../mock/store-response');
 
 describe('LiveRouter:', function() {
   require('./router-test')(LiveRouter);
+  
+  describe('live functionality:', function() {
+    var dispatcher, router, storeResponses, onData;
+    beforeEach(function() {
+      storeResponses = {
+        a: MockStoreResponse('value A'),
+        b: MockStoreResponse('value B'),
+        c: MockStoreResponse('value C')
+      };
+      dispatcher = new MockDispatcher;
+      dispatcher.dispatch.returns(storeResponses);
+      onData = sinon.spy();
+      router = new LiveRouter(dispatcher, onData);
+      router.addRoute('/a', ['a', 'b']);
+      router.addRoute('/b', ['b', 'c']);
+    });
+
+    it('calls the `onData()` callback when store responses publish new data', function(done) {
+      router.handleUrl('/a', null, function() {
+        var valueA = 'new value A', valueB = 'new value B';
+        storeResponses.a.publishUpdate(valueA);
+        storeResponses.b.publishUpdate(valueB);
+
+        expect(onData).toHaveBeenCalledWithMatch({a: valueA});
+        expect(onData).not.toHaveBeenCalledWithMatch({b: valueB});
+
+        done();
+      });
+    });
+  });
 });
